2797d189c8daae6af56c1ae9a581e9492f2ceb7f
YannikMahler <yannik.mahler1@outlook.de>
2022-02-21 20:25:56 +0100

    1

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import pygame
 import math
 
 pygame.init()
-
+ICON = pygame.image.load('favicon.ico')
 HEIGHT , WIDTH = 800,800
 WIN = pygame.display.set_mode((HEIGHT, WIDTH))
 pygame.display.set_caption("Planetensystem Simulation")
+pygame.display.set_icon(ICON)
 
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
@@ -13,10 +14,13 @@ BLUE = (100 ,149, 237)
 RED = (188, 39 ,50)
 DARK_GREY = (80, 78, 81) 
 
+
+FONT = pygame.font.SysFont("roboto", 16)
+
 class Planet: 
     AU = 149.6e6 * 1000  # Astronomical Units in meter
     G = 6.67428e-11 # Gravitionalconstant
-    SCALE = 250 / AU # 1AU = 100 pixels
+    SCALE = 220 / AU # 1AU = 100 pixels
     TIMESTEP = 3600*24 # 3600s = 1h * 24 = 1 day
     
     
@@ -38,8 +42,23 @@ class Planet:
         x = self.x * self.SCALE + WIDTH / 2
         y = self.y * self.SCALE + HEIGHT / 2
         
+        if len(self.orbit) > 2:
+            updated_points = []
+            for point in self.orbit:
+                x, y = point
+                x = x * self.SCALE + WIDTH / 2
+                y = y * self.SCALE + HEIGHT / 2
+                updated_points.append((x, y))
+            
+            pygame.draw.lines(win , self.color, False, updated_points, 2)
+        
+            
         pygame.draw.circle(win, self.color, (x,y), self.radius)
         
+        if not self.sun:
+            distance_text = FONT.render(f"{round(self.distance_to_sun / 1000, 1)}km", 1, WHITE)
+            win.blit(distance_text, (x - distance_text.get_width() / 2, y - distance_text.get_height() / 2))
+    
     def attraction(self, other):
         other_x, other_y = other.x, other.y
         distance_x = other_x - self.x
@@ -49,7 +68,7 @@ class Planet:
         if other.sun:
             self.distance_to_sun = distance
         
-        force = self.G * other.mass / distance**2
+        force = self.G * self.mass * other.mass / distance**2
         theta = math.atan2(distance_y, distance_x)
         force_x = math.cos(theta) * force
         force_y = math.sin(theta) * force
@@ -80,28 +99,31 @@ def main():
     sun.sun = True
     
     mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 0.330 * 10**24)
+    mercury.y_vel = -47.7 * 1000
     
     venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+    venus.y_vel = -35.02 * 1000
     
     earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+    earth.y_vel = 29.783 * 1000
     
     mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+    mars.y_vel = 24.077 * 1000
     
     planets = [sun, earth, mars, mercury, venus]
     
     while run:
         clock.tick(60)
-        #WIN.fill(WHITE)
-        #pygame.display.update()
+        WIN.fill((0, 0, 0))
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
         for planet in planets:
-            planet.draw(WIN )
+            planet.update_position(planets)
+            planet.draw(WIN)
             
         pygame.display.update()
-    pygame.quit()
-    
-main()
+    pygame.quit()    
+main() 
